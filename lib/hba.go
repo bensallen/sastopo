8b64842129ee576b5fef091c18a05a7e03108b3f
Ben Allen <bsallen@alcf.anl.gov>
2016-05-22 15:46:56 -0500

    Add comments to struct attributes

--- lib/hba.go
+++ lib/hba.go
@@ -2,7 +2,7 @@ package sastopo
 
 // HBA is a PCI SAS Host-bus Adapter
 type HBA struct {
-	PciID string
-	Host  int
-	Slot  string
+	PciID string // PCI Bus ID
+	Host  string // SCSI Host ID
+	Slot  string // Label that describes physical location
 }
