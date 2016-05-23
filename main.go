53af0bac3c6f1b684632a48210ad92aa25884fcb
Ben Allen <bsallen@alcf.anl.gov>
2016-05-22 23:21:37 -0500

    Add more testing prints

--- main.go
+++ main.go
@@ -6,13 +6,24 @@ import (
 	"gitlab.alcf.anl.gov/jlse/sastopo/lib"
 )
 
+func findDevMissingPaths(count int, devices map[string]*sastopo.Device) {
+	for _, d := range devices {
+		if len(d.MultiPath.Paths) != count {
+			fmt.Printf("Path Count Mismatch: %#v, found %d paths\n", d.Serial, len(d.MultiPath.Paths))
+		}
+	}
+}
+
 func main() {
-	devices, err := sastopo.ScsiDevices()
+	devices, _, HBAs, err := sastopo.ScsiDevices()
 	if err != nil {
 		fmt.Print(err)
 	}
 	for _, device := range devices {
-		fmt.Printf("Found Device: %#v \n", device.Serial)
+		fmt.Printf("Found Device: %#v \n", device.HBA)
 	}
+	fmt.Printf("Found HBAs: %#v \n", HBAs)
+
+	findDevMissingPaths(2, devices)
 
 }
